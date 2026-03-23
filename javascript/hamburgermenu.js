10f60dcec53f1ff66e635ed1cfe6d0ebb1d5f4bd
d3v1n33 <6067391@mborijnland.nl>
2026-03-23 13:09:20 +0100

    branch

---
+++ javascript/hamburgermenu.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    let hamburgerMenu = document.getElementById("myLinks")
+    if (hamburgerMenu.style.display === "block") {
+        hamburgerMenu.style.display = "none"
+    } else {
+        hamburgerMenu.style.display = "block"
+    }
+}
+
